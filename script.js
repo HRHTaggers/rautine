cdf90ae6491ca885105d1e45c2eff15044ba4413
HRHTaggers <99132424+HRHTaggers@users.noreply.github.com>
2022-09-19 22:17:50 +0100

    Implemented base HTML/JS structure & styling
    Significant refactoring needed

--- script.js
+++ script.js
@@ -9,12 +9,30 @@ const dropdownContainerTimingNow = document.querySelector(`.routine-input__form-
 const dropdownContainerTimingNext = document.querySelector(`.routine-input__form--timing-next`);
 const dropdownContainerTimingThen = document.querySelector(`.routine-input__form--timing-then`);
 const dropdownContainersAll = document.querySelectorAll(`.routine-input__form--dropdown-container`);
+const timelineDropdownActivity = document.querySelector(`.timeline__dropdown-activity`);
 const headerDate = document.querySelector(`.header__date`);
+
+//Transition Panel
 const activityIconNow = document.querySelector(`.transition-panel__icon--now`);
 const activityDescNow = document.querySelector(`.transition-panel__description--now`);
 const timeRemainingNow = document.querySelector(`.transition-panel__time--now`);
 const timingNow = document.querySelector(`.routine-input__form--timing-now`);
-const submitBtnNow = document.querySelector(`.routine-input__form--btn-now`);
+
+const activityIconNext = document.querySelector(`.transition-panel__icon--next`);
+const activityDescNext = document.querySelector(`.transition-panel__description--next`);
+const timeRemainingNext = document.querySelector(`.transition-panel__time--next`);
+const timingNext = document.querySelector(`.routine-input__form--timing-next`);
+
+const activityIconThen = document.querySelector(`.transition-panel__icon--then`);
+const activityDescThen = document.querySelector(`.transition-panel__description--then`);
+const timeRemainingThen = document.querySelector(`.transition-panel__time--then`);
+const timingThen = document.querySelector(`.routine-input__form--timing-then`);
+
+//Buttons
+const submitBtn = document.querySelector(`.routine-input__form--btn`);
+
+//Forms
+const formInput = document.querySelector(`.routine-input__form`);
 
 //DATA OBJECTS
 const activityData = [
@@ -24,7 +42,22 @@ const activityData = [
     icon: `<i class="fa-solid fa-apple-whole"></i>`,
   },
   {
+    name: "Lunch",
+    category: "Eat",
+    icon: `<i class="fa-solid fa-utensils"></i>`,
+  },
+  {
+    name: "Tea",
+    category: "Eat",
+    icon: `<i class="fa-solid fa-utensils"></i>`,
+  },
+  {
     name: "Nap",
+    category: "Sleep",
+    icon: `<i class="fa-solid fa-bed"></i>`,
+  },
+  {
+    name: "Bedtime",
     category: "Sleep",
     icon: `<i class="fa-solid fa-bed"></i>`,
   },
@@ -33,6 +66,36 @@ const activityData = [
     category: "Play",
     icon: `<i class="fa-solid fa-music"></i>`,
   },
+  {
+    name: "Outdoor",
+    category: "Play",
+    icon: `<i class="fa-solid fa-tree"></i>`,
+  },
+  {
+    name: "Phonics",
+    category: "Learning",
+    icon: `<i class="fa-brands fa-readme"></i>`,
+  },
+  {
+    name: "Maths",
+    category: "Learning",
+    icon: `<i class="fa-solid fa-plus"></i>`,
+  },
+];
+
+const transitionData = [
+    {
+        name: `now`,
+        capital: `Now`
+    },
+    { 
+        name: `next`,
+        capital: `Next`
+    }, 
+    {
+        name: `then`,
+        capital: `Then`
+    }
 ];
 
 //SET TIME
@@ -54,7 +117,7 @@ setInterval(() => {
             `saturday`,
             `sunday`
         ];
-        const weekday = weekdays[(today.getDay() - 1)];
+        const weekday = weekdays[today.getDay() < 0 ? today.getDay() - 1 : 0];
         const date = today.getDate();
         const months = [
             "january",
@@ -80,29 +143,63 @@ setInterval(() => {
     }, 1000
 );
 
-
 //DYNAMIC DROPDOWN - ACTIVITIES
-const createDropdown = function (array, i, parentEl) {
+const createDropdown = function (array, i = 0, parentEl) {
+  for (i = 0; i < array.length; i++) {
+    let option = document.createElement(`option`);
+    option.value = array[i].name;
+    option.textContent = array[i].name;
+    option.classList.add(`option-${array[i].name}`);
+    parentEl.appendChild(option);
+  }
+};
 
-    for(i = 0; i < array.length; i++) {
-        let option = document.createElement(`option`);
-        option.value = array[i].name;
-        option.textContent = array[i].name;
-        option.classList.add(`option-${array[i].name}`);
-        parentEl.appendChild(option);
-    }
+
+//GENERATE FORMS FOR EACH TRANSITION
+/*const generateForm = function (array, i = 0, parentEl) {
+
+    for (i = 0; i < array.length; i++) {
+      let form = document.createElement(`form`);
+      form.innerHTML = `
+            <div class="col-sm-4">
+                <label class="routine-input__form--label">${array[i].name}</label>
+                <select name="dropdown" class="routine-input__form--dropdown-container routine-input__form--dropdown-container-activity-${array[i].name}">
+                    <option>Snack</option>
+                    <option>Lunch</option>
+                    <option>Nap</option>
+                    <option>Outdoor Play</option>
+                    <option>Music</option>
+                </select>
+                <input type="time" class="routine-input__form--timing routine-input__form--timing-now" />
+                <!--<button class="routine-input__form--btn-${array[i].name} btn">
+                    Add to routine
+                </button>-->
+            </div>
+        `;
+        parentEl.appendChild(form);
+    };
 
 };
+*/
 
 //SET ACTIVITY & TIMING IN PANEL FROM DROPDOWNS
 const selectActivity = function(event) {
   event.preventDefault();
 
-  //Update activity from dropdown selection in DOM - decription & icon
-  const selectedActivity = dropdownContainerActivityNow.value;
-  activityDescNow.innerHTML = selectedActivity;
+  //Update activity from dropdown selection in DOM - deacription & icon
+  const selectedActivityNow = dropdownContainerActivityNow.value;
+  activityDescNow.innerHTML = selectedActivityNow;
   activityIconNow.innerHTML = activityData[dropdownContainerActivityNow.selectedIndex].icon;
 
+  const selectedActivityNext = dropdownContainerActivityNext.value;
+  activityDescNext.innerHTML = selectedActivityNext;
+  activityIconNext.innerHTML =
+    activityData[dropdownContainerActivityNext.selectedIndex].icon;
+
+  const selectedActivityThen = dropdownContainerActivityThen.value;
+  activityDescThen.innerHTML = selectedActivityThen;
+  activityIconThen.innerHTML =
+    activityData[dropdownContainerActivityThen.selectedIndex].icon;
 
   //Update remaining time for activity
   //Set finish time from dropdown selection
@@ -122,19 +219,51 @@ const selectActivity = function(event) {
       //Calculate remaining time & update DOM
       const timeRemaining = totalMinutesEnd - totalMinutesNow;
       timeRemainingNow.innerHTML = `Time left: ${timeRemaining} minutes`;
+
+      const startTimeNext = dropdownContainerTimingNext.value;
+      timeRemainingNext.innerHTML = `Starts at: ${startTimeNext}`;
+
+      const startTimeThen = dropdownContainerTimingThen.value;
+      timeRemainingThen.innerHTML = `Starts at: ${startTimeThen}`;
     }
     setFinishTime();
   }, 1000);
-
 };
 
-submitBtnNow.addEventListener(`click`, selectActivity);
+//EVENT LISTENERS
+//submitBtnNow.addEventListener(`click`, selectActivity);
+//submitBtnNext.addEventListener(`click`, selectActivity);
+//submitBtnThen.addEventListener(`click`, selectActivity);
+submitBtn.addEventListener(`click`, selectActivity);
 
 //INITIALIZATION FUNCTION
-const init = function() {
+function init() {
     createDropdown(activityData, 0, dropdownContainerActivityNow);
     createDropdown(activityData, 0, dropdownContainerActivityNext);
     createDropdown(activityData, 0, dropdownContainerActivityThen);
 };
 
 init();
+
+/*
+//TIMELINE HTML
+//SET ACTIVITY IN TIMELINE
+const setTimelineActivity = function(event) {
+    event.preventDefault();
+
+
+}
+
+//SET PROGRESS IN TIMELINE
+const setTimelineColor = function(event) {
+    event.preventDefault();
+
+    //Get date & time
+        const today = new Date();
+        
+        //Convert date & time to string
+        const hours = today.getHours();
+        const minutes = today.getMinutes();
+
+};
+*/
