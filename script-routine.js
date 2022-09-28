726566d517ba3d5ea58a0cb203f929c91ffb0690
HRHTaggers <99132424+HRHTaggers@users.noreply.github.com>
2022-09-28 23:38:11 +0100

    Added daily routine html with d&d timeline

--- script-routine.js
+++ script-routine.js
@@ -1,6 +1,54 @@
-const dragStart = (event) => {
-  event.currentTarget.classList.add("dragging");
+/*const timeCheck = function () {
+  const hours = new Date().getHours();
+  const timeslot = document.querySelector(`.`)
+  console.log(hours);
+  if(hours === 22) 
 };
+
+timeCheck();
+*/
+
+//DOM REFERENCES
+const headerDate2 = document.querySelector(`.header__date2`);
+const timeslots = document.querySelectorAll(`.timeslot`);
+const activitySlots = document.querySelectorAll(`.activitySlot`);
+const completionSlots = document.querySelectorAll(`.completionSlot`);
+
+//TIME INDICATOR
+const indicateHour = function() {
+
+  const hours = new Date().getHours();
+  const hourMarkup = `${hours}:00`;
+  console.log(hours, hourMarkup);
+
+  timeslots.forEach(function(timeslot) {
+  if(timeslot.textContent === hourMarkup) {
+    timeslot.classList.add(`highlight`);
+    }
+  });
+
+};
+
+indicateHour();
+
+//COMPLETION INDICATOR
+const indicateCompleted = function() {
+
+  completionSlots.forEach(function(completionSlot) {
+    completionSlot.addEventListener("click", () => {
+      completionSlot.innerHTML = ``;
+      completionSlot.classList.toggle(`completed`);
+    });
+  });
+
+};
+
+indicateCompleted();
+
+//DRAG AND DROP
+function dragStart(event) {
+  event.currentTarget.classList.add("dragging");
+}
 
 const dragEnd = (event) => {
   event.currentTarget.classList.remove("dragging");
@@ -44,3 +92,50 @@ const drop = (event) => {
 const allowDrop = (event) => {
   event.preventDefault();
 };
+
+//SET TIME
+setInterval(() => {
+  function setDate() {
+    //Get date & time
+    const today = new Date();
+
+    //Convert date & time to string
+    const hours = today.getHours();
+    const minutes = today.getMinutes();
+    const weekdays = [
+      `monday`,
+      `tuesday`,
+      `wednesday`,
+      `thursday`,
+      `friday`,
+      `saturday`,
+      `sunday`,
+    ];
+    const weekday = weekdays[today.getDay() > 1 ? today.getDay() - 1 : 0];
+    const date = today.getDate();
+    const months = [
+      "january",
+      "february",
+      "march",
+      "april",
+      "may",
+      "june",
+      "july",
+      "august",
+      "september",
+      "october",
+      "november",
+      "december",
+    ];
+    const month = months[today.getMonth() - 1];
+    const year = today.getFullYear();
+
+    //Insert date & time to DOM
+    headerDate2.innerHTML = `${weekday}, ${date} ${month} ${year}, ${
+      hours < 12 ? `0` : ``
+    }${hours}:${minutes < 10 ? `0` : ``}${minutes}`;
+  }
+  setDate();
+}, 1000);
+
+setDate();
