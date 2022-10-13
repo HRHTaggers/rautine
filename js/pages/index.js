7a0fd2ca4eb9937ca24cc1328b1baf4482853da9
HRHTaggers <99132424+HRHTaggers@users.noreply.github.com>
2022-10-13 11:55:03 +0100

    Added index.js

---
+++ js/pages/index.js
@@ -0,0 +1,11 @@
+"use strict";
+
+import { headerView } from "../components/header.js";
+import { navView } from "../components/nav.js";
+import { footerView } from "../components/footer.js";
+
+headerView();
+
+navView();
+
+footerView();
