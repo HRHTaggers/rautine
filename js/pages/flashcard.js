37e45d60ab2dbeabc8dd85973e3683048c8a06f4
HRHTaggers <99132424+HRHTaggers@users.noreply.github.com>
2022-10-24 22:19:09 +0100

    Added recognition capacity to flashcard.js

--- js/pages/flashcard.js
+++ js/pages/flashcard.js
@@ -1,7 +1,7 @@
 "use strict";
 
-import {
-  flashcardDisplay, flashcardDropdown, displayFlashcardBtn, clearFlashcardBtn
+//import { selectedUser } from "./index.js";
+import { starAudio, flashcardPersonalizationHeading, flashcardDisplay, flashcardDropdown, displayFlashcardBtn, clearFlashcardBtn, personalizationSelect
 } from "../selectors.js";
 import { createDropdown } from "../components/dropdown.js";
 import { state } from "../components/model.js";
@@ -19,10 +19,33 @@ createDropdown(state.activityData, 0, flashcardDropdown);
 
 displayFlashcardBtn.addEventListener("click", function () {
   setVisual(flashcardDropdown, flashcardDisplay);
+  flashcardPersonalizationHeading.classList.remove(`hidden`);
+  flashcardPersonalizationHeading.innerHTML = `
+    <div class="flashcard-panel__instructions">tap on the picture to choose your activity</div>
+    <div class="flashcard-panel__instructions--symbol">👇</div>
+  `;
+});
+
+flashcardDisplay.addEventListener("click", function() {
+  flashcardDisplay.classList.remove(`hidden`);
+  flashcardDisplay.innerHTML = `
+  <div class="col display-panel__flashcard display-panel__flashcard--5" id="flashcard-display-panel-column">
+    <figure class="display-panel" id="display-panel-icon">
+      🌟
+    </figure>
+    <div class="display-panel__title" id="display-panel-title">
+      great! let's start
+    </div>
+  </div>
+  `;
+  starAudio.play();
 });
 
 clearFlashcardBtn.addEventListener("click", function () {
   clearVisuals(flashcardDisplay);
+  clearVisuals(flashcardPersonalizationHeading);
+  flashcardPersonalizationHeading.classList.add(`hidden`);
+  flashcardDisplay.classList.add(`hidden`);
 });
 
 footerView();
